849ed847e13d508b1ee74425812596c84ae9d31b
Mehul Varshney <varshneymehul5@gmail.com>
2021-05-04 21:53:53 +0530

    Fixing bugs #2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Configuring ENV
-const dotenv = require("dotenv");
-dotenv.config();
+ if (process.env.NODE_ENV !== "production") {
+   require("dotenv").config();
+ }
 
 // App config
 const express = require("express");
